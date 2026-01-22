76314db6bad5c96591aeed584b9cb13c1b1d0493
rpbArpitKhandelwal <arpit112005kh@gmail.com>
2026-01-23 02:04:33 +0530

    CF: 2112A - Race (Accepted)

---
+++ CF/Race_2112A.java
@@ -0,0 +1,8 @@
+// Platform: Codeforces
+// Problem: 2112A - Race
+// Submission ID: 325705352
+// Status: Accepted
+// URL: https://codeforces.com/contest/2112/submission/325705352
+
+// TODO:
+// Paste your accepted Java solution here.
