869e046d3d93d2481c81f092ea01076d4aaecab7
rpbArpitKhandelwal <arpit112005kh@gmail.com>
2026-01-23 02:04:31 +0530

    CF: 2171C1 - Renako Amaori and XOR Game (easy version) (Accepted)

---
+++ CF/Renako_Amaori_and_XOR_Game_easy_version_2171C1.java
@@ -0,0 +1,8 @@
+// Platform: Codeforces
+// Problem: 2171C1 - Renako Amaori and XOR Game (easy version)
+// Submission ID: 349971252
+// Status: Accepted
+// URL: https://codeforces.com/contest/2171/submission/349971252
+
+// TODO:
+// Paste your accepted Java solution here.
