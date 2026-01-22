9c7c44e77afde437069335a70b608acad609ee7e
rpbArpitKhandelwal <arpit112005kh@gmail.com>
2026-01-23 02:04:31 +0530

    CF: 2173A - Sleeping Through Classes (Accepted)

---
+++ CF/Sleeping_Through_Classes_2173A.java
@@ -0,0 +1,8 @@
+// Platform: Codeforces
+// Problem: 2173A - Sleeping Through Classes
+// Submission ID: 352080144
+// Status: Accepted
+// URL: https://codeforces.com/contest/2173/submission/352080144
+
+// TODO:
+// Paste your accepted Java solution here.
