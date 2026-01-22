8ab79850729f52528e7e1c523deba234824013fc
rpbArpitKhandelwal <arpit112005kh@gmail.com>
2026-01-23 02:04:32 +0530

    CF: 2131A - Lever (Accepted)

---
+++ CF/Lever_2131A.java
@@ -0,0 +1,8 @@
+// Platform: Codeforces
+// Problem: 2131A - Lever
+// Submission ID: 333289219
+// Status: Accepted
+// URL: https://codeforces.com/contest/2131/submission/333289219
+
+// TODO:
+// Paste your accepted Java solution here.
