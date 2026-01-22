f11589050bb7f06379aedd51a039c4f4b4202279
rpbArpitKhandelwal <arpit112005kh@gmail.com>
2026-01-23 02:04:31 +0530

    CF: 2180B - Ashmal (Accepted)

---
+++ CF/Ashmal_2180B.java
@@ -0,0 +1,8 @@
+// Platform: Codeforces
+// Problem: 2180B - Ashmal
+// Submission ID: 354197954
+// Status: Accepted
+// URL: https://codeforces.com/contest/2180/submission/354197954
+
+// TODO:
+// Paste your accepted Java solution here.
