0bdc9680af7522968ee6f34d67d9f5a28b9a8082
rpbArpitKhandelwal <arpit112005kh@gmail.com>
2026-01-23 02:04:33 +0530

    CF: 2110B - Down with Brackets (Accepted)

---
+++ CF/Down_with_Brackets_2110B.java
@@ -0,0 +1,8 @@
+// Platform: Codeforces
+// Problem: 2110B - Down with Brackets
+// Submission ID: 321097109
+// Status: Accepted
+// URL: https://codeforces.com/contest/2110/submission/321097109
+
+// TODO:
+// Paste your accepted Java solution here.
