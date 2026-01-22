d103bec67f3592b9161ea333400049bf00e45167
rpbArpitKhandelwal <arpit112005kh@gmail.com>
2026-01-23 02:04:31 +0530

    CF: 2185A - Perfect Root (Accepted)

---
+++ CF/Perfect_Root_2185A.java
@@ -0,0 +1,8 @@
+// Platform: Codeforces
+// Problem: 2185A - Perfect Root
+// Submission ID: 358628205
+// Status: Accepted
+// URL: https://codeforces.com/contest/2185/submission/358628205
+
+// TODO:
+// Paste your accepted Java solution here.
