bdd8a8b9f683b26cec46dd2380f0888d80cb11e0
rpbArpitKhandelwal <arpit112005kh@gmail.com>
2026-01-23 02:04:32 +0530

    CF: 2171A - Shizuku Hoshikawa and Farm Legs (Accepted)

---
+++ CF/Shizuku_Hoshikawa_and_Farm_Legs_2171A.java
@@ -0,0 +1,8 @@
+// Platform: Codeforces
+// Problem: 2171A - Shizuku Hoshikawa and Farm Legs
+// Submission ID: 349922531
+// Status: Accepted
+// URL: https://codeforces.com/contest/2171/submission/349922531
+
+// TODO:
+// Paste your accepted Java solution here.
