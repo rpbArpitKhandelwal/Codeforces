a8bbefe6a7ea8f36250f59d78ae25f80a5c40998
rpbArpitKhandelwal <arpit112005kh@gmail.com>
2026-01-23 02:04:31 +0530

    CF: 1832B - Maximum Sum (Accepted)

---
+++ CF/Maximum_Sum_1832B.java
@@ -0,0 +1,8 @@
+// Platform: Codeforces
+// Problem: 1832B - Maximum Sum
+// Submission ID: 358877037
+// Status: Accepted
+// URL: https://codeforces.com/contest/1832/submission/358877037
+
+// TODO:
+// Paste your accepted Java solution here.
