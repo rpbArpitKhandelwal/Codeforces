b4e1bbaa67bd6217077177ddb176df156c9d5342
rpbArpitKhandelwal <arpit112005kh@gmail.com>
2026-01-23 02:04:32 +0530

    CF: 2132D - From 1 to Infinity (Accepted)

---
+++ CF/From_1_to_Infinity_2132D.java
@@ -0,0 +1,8 @@
+// Platform: Codeforces
+// Problem: 2132D - From 1 to Infinity
+// Submission ID: 334921877
+// Status: Accepted
+// URL: https://codeforces.com/contest/2132/submission/334921877
+
+// TODO:
+// Paste your accepted Java solution here.
