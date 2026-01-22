290555f1da87f53cb46eac88a78c45b81ba4c9e7
rpbArpitKhandelwal <arpit112005kh@gmail.com>
2026-01-23 02:04:31 +0530

    CF: 2185C - Shifted MEX (Accepted)

---
+++ CF/Shifted_MEX_2185C.java
@@ -0,0 +1,8 @@
+// Platform: Codeforces
+// Problem: 2185C - Shifted MEX
+// Submission ID: 358633389
+// Status: Accepted
+// URL: https://codeforces.com/contest/2185/submission/358633389
+
+// TODO:
+// Paste your accepted Java solution here.
