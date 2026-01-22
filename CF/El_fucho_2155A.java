4e63a0ea6a7568aafa2f360550294f2e7634f829
rpbArpitKhandelwal <arpit112005kh@gmail.com>
2026-01-23 02:04:32 +0530

    CF: 2155A - El fucho (Accepted)

---
+++ CF/El_fucho_2155A.java
@@ -0,0 +1,8 @@
+// Platform: Codeforces
+// Problem: 2155A - El fucho
+// Submission ID: 342055447
+// Status: Accepted
+// URL: https://codeforces.com/contest/2155/submission/342055447
+
+// TODO:
+// Paste your accepted Java solution here.
