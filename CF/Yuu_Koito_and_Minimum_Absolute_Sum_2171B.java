2bb2b5e3352c2a84aca066738cd6c695cace9e7f
rpbArpitKhandelwal <arpit112005kh@gmail.com>
2026-01-23 02:04:31 +0530

    CF: 2171B - Yuu Koito and Minimum Absolute Sum (Accepted)

---
+++ CF/Yuu_Koito_and_Minimum_Absolute_Sum_2171B.java
@@ -0,0 +1,8 @@
+// Platform: Codeforces
+// Problem: 2171B - Yuu Koito and Minimum Absolute Sum
+// Submission ID: 349968460
+// Status: Accepted
+// URL: https://codeforces.com/contest/2171/submission/349968460
+
+// TODO:
+// Paste your accepted Java solution here.
