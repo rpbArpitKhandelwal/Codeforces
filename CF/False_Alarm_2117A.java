490698635419dd7127ff32f171d34ca596a53316
rpbArpitKhandelwal <arpit112005kh@gmail.com>
2026-01-23 02:04:33 +0530

    CF: 2117A - False Alarm (Accepted)

---
+++ CF/False_Alarm_2117A.java
@@ -0,0 +1,8 @@
+// Platform: Codeforces
+// Problem: 2117A - False Alarm
+// Submission ID: 323500849
+// Status: Accepted
+// URL: https://codeforces.com/contest/2117/submission/323500849
+
+// TODO:
+// Paste your accepted Java solution here.
