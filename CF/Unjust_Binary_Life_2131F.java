d9a92f17df61edc0753d75cb5a17c08b874c4e4c
rpbArpitKhandelwal <arpit112005kh@gmail.com>
2026-01-23 02:04:32 +0530

    CF: 2131F - Unjust Binary Life (Accepted)

---
+++ CF/Unjust_Binary_Life_2131F.java
@@ -0,0 +1,8 @@
+// Platform: Codeforces
+// Problem: 2131F - Unjust Binary Life
+// Submission ID: 333357061
+// Status: Accepted
+// URL: https://codeforces.com/contest/2131/submission/333357061
+
+// TODO:
+// Paste your accepted Java solution here.
