937a37de38a48e626836167d844758febd5b31c3
rpbArpitKhandelwal <arpit112005kh@gmail.com>
2026-01-23 02:04:32 +0530

    CF: 2132A - Homework (Accepted)

---
+++ CF/Homework_2132A.java
@@ -0,0 +1,8 @@
+// Platform: Codeforces
+// Problem: 2132A - Homework
+// Submission ID: 334880196
+// Status: Accepted
+// URL: https://codeforces.com/contest/2132/submission/334880196
+
+// TODO:
+// Paste your accepted Java solution here.
