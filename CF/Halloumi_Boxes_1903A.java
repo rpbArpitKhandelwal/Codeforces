25428c0977c394070a7c89497f1757ca65484d1a
rpbArpitKhandelwal <arpit112005kh@gmail.com>
2026-01-23 02:04:32 +0530

    CF: 1903A - Halloumi Boxes (Accepted)

---
+++ CF/Halloumi_Boxes_1903A.java
@@ -0,0 +1,8 @@
+// Platform: Codeforces
+// Problem: 1903A - Halloumi Boxes
+// Submission ID: 344105541
+// Status: Accepted
+// URL: https://codeforces.com/contest/1903/submission/344105541
+
+// TODO:
+// Paste your accepted Java solution here.
