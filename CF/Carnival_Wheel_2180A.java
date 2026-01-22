cb09e04d48738f6fcd9ceaa7c1f9dd6fb4b28af1
rpbArpitKhandelwal <arpit112005kh@gmail.com>
2026-01-23 02:04:31 +0530

    CF: 2180A - Carnival Wheel (Accepted)

---
+++ CF/Carnival_Wheel_2180A.java
@@ -0,0 +1,8 @@
+// Platform: Codeforces
+// Problem: 2180A - Carnival Wheel
+// Submission ID: 354194025
+// Status: Accepted
+// URL: https://codeforces.com/contest/2180/submission/354194025
+
+// TODO:
+// Paste your accepted Java solution here.
