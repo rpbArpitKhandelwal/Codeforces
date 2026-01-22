f2c841fd4069a93bfc38eb95ebd8f4c77aa62096
rpbArpitKhandelwal <arpit112005kh@gmail.com>
2026-01-23 02:04:32 +0530

    CF: 2137A - Collatz Conjecture (Accepted)

---
+++ CF/Collatz_Conjecture_2137A.java
@@ -0,0 +1,8 @@
+// Platform: Codeforces
+// Problem: 2137A - Collatz Conjecture
+// Submission ID: 337423436
+// Status: Accepted
+// URL: https://codeforces.com/contest/2137/submission/337423436
+
+// TODO:
+// Paste your accepted Java solution here.
