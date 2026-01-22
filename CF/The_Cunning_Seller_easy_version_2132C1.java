bf66a6704c89f6d50036aff9fe896ca447e777c0
rpbArpitKhandelwal <arpit112005kh@gmail.com>
2026-01-23 02:04:32 +0530

    CF: 2132C1 - The Cunning Seller (easy version) (Accepted)

---
+++ CF/The_Cunning_Seller_easy_version_2132C1.java
@@ -0,0 +1,8 @@
+// Platform: Codeforces
+// Problem: 2132C1 - The Cunning Seller (easy version)
+// Submission ID: 334900038
+// Status: Accepted
+// URL: https://codeforces.com/contest/2132/submission/334900038
+
+// TODO:
+// Paste your accepted Java solution here.
