0187df74e1a4de573093720bd15b15e2fd2c8763
rpbArpitKhandelwal <arpit112005kh@gmail.com>
2026-01-23 02:04:32 +0530

    CF: 2137C - Maximum Even Sum (Accepted)

---
+++ CF/Maximum_Even_Sum_2137C.java
@@ -0,0 +1,8 @@
+// Platform: Codeforces
+// Problem: 2137C - Maximum Even Sum
+// Submission ID: 337437615
+// Status: Accepted
+// URL: https://codeforces.com/contest/2137/submission/337437615
+
+// TODO:
+// Paste your accepted Java solution here.
