6845cfbdaf544aeebe0c96b2940cdb8eb5b1ec81
rpbArpitKhandelwal <arpit112005kh@gmail.com>
2026-01-23 02:04:32 +0530

    CF: 2132B - The Secret Number (Accepted)

---
+++ CF/The_Secret_Number_2132B.java
@@ -0,0 +1,8 @@
+// Platform: Codeforces
+// Problem: 2132B - The Secret Number
+// Submission ID: 334889889
+// Status: Accepted
+// URL: https://codeforces.com/contest/2132/submission/334889889
+
+// TODO:
+// Paste your accepted Java solution here.
