1990bd0e154d75a191a8d2e6b31a7ecbb8c73710
rpbArpitKhandelwal <arpit112005kh@gmail.com>
2026-01-23 02:04:31 +0530

    CF: 2185B - Prefix Max (Accepted)

---
+++ CF/Prefix_Max_2185B.java
@@ -0,0 +1,8 @@
+// Platform: Codeforces
+// Problem: 2185B - Prefix Max
+// Submission ID: 358631496
+// Status: Accepted
+// URL: https://codeforces.com/contest/2185/submission/358631496
+
+// TODO:
+// Paste your accepted Java solution here.
