9ee497324ac67e89a35185fb7df12e93758f5ffb
Arpit Khandelwal <156059029+rpbArpitKhandelwal@users.noreply.github.com>
2026-01-25 19:37:11 +0530

    Add accepted solution for Codeforces 2180B
    Implemented the solution for Codeforces problem 2180.

--- CF/Ashmal_2180B.java
+++ CF/Ashmal_2180B.java
@@ -5,4 +5,33 @@
 // URL: https://codeforces.com/contest/2180/submission/354197954
 
 // TODO:
-// Paste your accepted Java solution here.
+import java .util.*;
+public class Ashmal {
+ 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            String s = "";
+            for (int i = 0; i < n; i++) {
+                String str = sc.next();
+                if(s.isEmpty()){
+                    s = str;
+                } 
+                else{
+                    String first = str + s;
+                    String last = s + str;
+                    if(first.compareTo(last) < 0){
+                        s = first;
+                    } 
+                    else{
+                        s = last;
+                    }
+                }
+            }
+            System.out.println(s);
+        }
+        sc.close();
+    }
+}
