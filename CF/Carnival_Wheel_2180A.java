64a15ea65bcf1b5f1f9fbbb3c7ab518aa036c62f
Arpit Khandelwal <156059029+rpbArpitKhandelwal@users.noreply.github.com>
2026-02-07 00:07:49 +0530

    Add solution for People With Secret problem
    Implemented the solution for the People With Secret problem.

--- CF/Carnival_Wheel_2180A.java
+++ CF/Carnival_Wheel_2180A.java
@@ -5,4 +5,36 @@
 // URL: https://codeforces.com/contest/2180/submission/354194025
 
 // TODO:
-// Paste your accepted Java solution here.
+
+import java .util.*;
+public class People_With_Secret {
+ 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int l = sc.nextInt();
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int k = gcd(l, b);
+            int len = l / k;
+            int posi = a;
+            int maxPrize = posi;
+            for (int i = 0; i < len; i++) {
+                maxPrize = Math.max(maxPrize, posi);
+                posi = (posi + b) % l;
+            }
+            System.out.println(maxPrize);
+        }
+        sc.close();
+    }
+    
+    public static int gcd(int x, int y) {
+        while (y != 0) {
+            int temp = x % y;
+            x = y;
+            y = temp;
+        }
+        return x;
+    }
+}
