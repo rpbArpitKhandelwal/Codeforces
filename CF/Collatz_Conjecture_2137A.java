3a0505ecd3ec09ef504a052751ebb5b5aea78f39
Arpit Khandelwal <156059029+rpbArpitKhandelwal@users.noreply.github.com>
2026-02-07 23:54:34 +0530

    Implement Collatz Conjecture solution in Java

--- CF/Collatz_Conjecture_2137A.java
+++ CF/Collatz_Conjecture_2137A.java
@@ -5,4 +5,19 @@
 // URL: https://codeforces.com/contest/2137/submission/337423436
 
 // TODO:
-// Paste your accepted Java solution here.
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int k = sc.nextInt();
+            int x = sc.nextInt();
+            for (int i = 0; i < k; i++) {
+                x *= 2;
+            }
+            
+            System.out.println(x);
+        }
+    }
+}
