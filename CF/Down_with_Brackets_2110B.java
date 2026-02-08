85d1cc4357db5e75c22ec4b8164e66b69a913120
Arpit Khandelwal <156059029+rpbArpitKhandelwal@users.noreply.github.com>
2026-02-08 08:28:09 +0530

    Add Java solution for Down with Brackets problem
    Implement solution for Codeforces problem 2110B.

--- CF/Down_with_Brackets_2110B.java
+++ CF/Down_with_Brackets_2110B.java
@@ -5,4 +5,43 @@
 // URL: https://codeforces.com/contest/2110/submission/321097109
 
 // TODO:
-// Paste your accepted Java solution here.
+
+import java.util.*;
+public class B_2110 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            String s = sc.next();
+            if (canBreakString(s)) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+        sc.close();
+    }
+    public static boolean canBreakString(String s) {
+        if (isNested(s)) {
+            return false;
+        }
+        return true;
+    }
+    public static boolean isNested(String s) {
+        int n = s.length();
+        int balance = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == '(') {
+                balance++;
+            } else {
+                balance--;
+            }
+            if (balance == 0 && i != n - 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
