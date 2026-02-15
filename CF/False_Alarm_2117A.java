fe2064465ab85d376dcb20e18a2011694063dfa1
Arpit Khandelwal <156059029+rpbArpitKhandelwal@users.noreply.github.com>
2026-02-15 09:56:38 +0530

    Implement Java solution for Codeforces problem 2117A

--- CF/False_Alarm_2117A.java
+++ CF/False_Alarm_2117A.java
@@ -5,4 +5,47 @@
 // URL: https://codeforces.com/contest/2117/submission/323500849
 
 // TODO:
-// Paste your accepted Java solution here.
+import java.util.*;
+public class A_2117 {
+
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt(); // Number of test cases
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int x = sc.nextInt();
+            int[] doors = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                doors[i] = sc.nextInt();
+            }
+
+            boolean canPass = false;
+
+            // Try pressing the button at every time from 0 to n
+            for (int startTime = 0; startTime <= n; startTime++) {
+                boolean possible = true;
+
+                for (int i = 0; i < n; i++) {
+                    // If door is open, or within the effect of button
+                    if (doors[i] == 0) continue;
+                    if (i >= startTime && i < startTime + x) continue;
+
+                    // Otherwise, the door is closed and not within the effect
+                    possible = false;
+                    break;
+                }
+
+                if (possible) {
+                    canPass = true;
+                    break;
+                }
+            }
+
+            System.out.println(canPass ? "YES" : "NO");
+        }
+
+        sc.close();
+    }
+}
